511c67f61b5975dd20a6c54941c39b2961213c4f
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-03-03 18:57:26 -0700

    models routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :controllerName
+  # custom route, http verb '/whatever', to: 'controller#action',
+  # root '' where to go on initial landing
+  root 'subs#index'
+
+  # get '/subs', to: 'subs#index'
+  # get '/sub', to: 'subs#show'
+  # get '/subs/:id', to: 'subs#edit'
+
+  resources :subs do
+    resources :topics 
+  end
+
+  # resources :topics do
+  #   resources :comments
+  # end
+
+  # resources :comments do
+  #   resources :likes
+  # end
+
 end
