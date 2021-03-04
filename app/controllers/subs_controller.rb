6632a3be48d2612d247d72efbe9a05fac2494362
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-03-03 20:26:59 -0700

    filled out controllers

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,13 +1,123 @@
 class SubsController < ApplicationController
+  # Skinny Controllers / fat model
+  # Action - items on your webpage
+  # C.R.U.D
+  # Create -> POST
+  # Read -> GET
+  # Update -> PUT / PATCH
+  # Destroy -> DELETE
+  # convention over configuration
+  # pattern learn
+  # a controller action must return HTML, JSON, XML, redirect action that does
+
+  # GET / READ
+  # index - view all of the records in the table
+    # def index
+    #   @model_names = Model_name.all
+    #   render component: "ModelNames", props: { model_names: @model_names:}
+    # end
+
+  # show - needs id, show a single record in the table
+    # def show
+    #   @model_name = Model_name.find(params[:id])
+    #   render component: "ModelName", props: { model_name: @model_name }
+    # end
+
+  # new - create an obj in memory, have shown the new form
+    # def new
+    #   @model_name = Model_name.new
+    #   render component: "ModelNameNew", props: { model_name: @model_name}
+    # end
+
+  # edit - needs id, show teh edit form
+    # def edit
+    #   @model_name = Model_name.find(params[:id])
+    #   render component: "ModelNameEdit", props: { model_name: @model_name}
+    # end
+
+  # Create / POST
+    # - take the data in the form and create in the db
+    # def create
+    #   @model_name = Model_name.new(model_name_params)
+    #   if @model_name.save
+    #     # render or redirect
+    #   else
+    #     render component: 'ModelNameNew', props: { model_name: @model_name }
+    #     render :new
+    #   end
+    # end
+
+    # private
+    #   def model_name_params
+    #     params.require(:model_name).permit(:attr, :attr2, :everythingTheTableHas)
+    #   end
+
+  # Update / PUT
+    # -take values from teh edit form and it will find the record with id, update the record in the db
+    # def update
+    #   @model_name = Model_name.find(params[:id])
+    #   if @model_name.update(model_name_params)
+    #     # render or redirect
+    #   else
+    #     render component: 'ModelNameEdit', props: { model_name: @model_name }
+    #   end
+    # end
+
+  # Destroy / DELETE
+      # - find the record with the given id, delete the record in the db
+      # def destroy
+      #   @model_name = Model_name.find(params[:id])
+      #   @model_name.destroy
+      #   # redirect to
+      # end
+
   def index
+    subs = Sub.all
+    render component: 'Subs', props: { subs: @subs }
   end
 
   def show
+    @sub = Sub.find(params[:id])
+    render component: 'Sub', props: { sub: @sub }
   end
 
   def new
+    @sub = Sub.new
+    render component: 'SubNew', props: { sub: @sub }
+  end
+
+  def create
+    @sub = Sub.new(sub_params)
+    if @sub.save
+      redirect_to subs_path
+    else
+      render component: 'SubNew', props: { sub: @sub }
+    end
   end
 
   def edit
+    @sub = Sub.find(params[:id])
+    render component: 'SubEdit', props: { sub: @sub }
   end
+
+  def update
+    @sub = Sub.find(params[:id])
+    if @sub.update(sub_params)
+      redirect_to subs_path
+    else
+      render component: 'SubEdit', props: { sub: @sub }
+    end
+  end
+
+  def destroy
+    @sub = Sub.find(params[:id])
+    @sub.destroy
+    redirect_to subs_path
+  end
+
+  private
+    def sub_params
+      params.require(:sub).permit(:title)
+    end
+
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,13 +1,48 @@
 class TopicsController < ApplicationController
+  before_action :set_sub 
+  before_action :set_topic, only: [ :show, :edit, :update, :destroy ]
   def index
+    @topics = @sub.topics
+    render component: 'Topics', props: { sub: @sub, topics: @topics }
   end
-
   def show
+    render component: 'Topic', props: { sub: @sub, topic: @topic }
   end
-
   def new
+    @topic = @sub.topics.new 
+    render component: 'TopicNew', props: { sub: @sub, topic: @topic }
   end
-
+  def create
+    @topic = @sub.topics.new(topic_params)
+    if @topic
+      # redirect_to [@sub, @topic]
+      redirect_to sub_topics_path(@sub)
+    else
+      render component: 'TopicNew', props: { sub: @sub, topic: @topic }
+    end
+  end
   def edit
+    render component: 'TopicEdit', props: { sub: @sub, topic: @topic }
   end
+  def update
+    if @topic.update(topic_params)
+      redirect_to sub_topics_path(@sub)
+    else
+      render component: 'TopicEdit', props: { sub: @sub, topic: @topic}
+    end
+  end
+  def destroy
+    @topic.destroy
+    redirect_to sub_topics_path(@sub)
+  end
+  private 
+    def topic_params
+      params.require(:topic).permit(:name, :body)
+    end
+    def set_sub
+      @sub = Sub.find(params[:sub_id])
+    end
+    def set_topic
+      @topic = @sub.topics.find(params[:id])
+    end
 end
